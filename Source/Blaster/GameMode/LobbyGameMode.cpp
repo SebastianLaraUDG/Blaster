b5402838e5ad7c52307c8ea27acd24534cae52dd
Sebastian L <sebastian.lara6087@alumnos.udg.mx>
2025-12-11 14:11:54 -0600

    Comments in lobby game mode

--- Source/Blaster/GameMode/LobbyGameMode.cpp
+++ Source/Blaster/GameMode/LobbyGameMode.cpp
@@ -9,8 +9,9 @@ void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
 {
 	Super::PostLogin(NewPlayer);
 	
+	// Change from lobby to Gameplay level if we have enough players./
 	int32 NumberOfPlayers = GameState.Get()->PlayerArray.Num();
-	if (NumberOfPlayers == 2)
+	if (NumberOfPlayers == 2) // TODO: 2 is not strictly necessary.
 	{
 		if (UWorld* const World = GetWorld())
 		{
