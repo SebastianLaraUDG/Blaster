ed510562765971bc300dd78d6f95326693adb474
Sebastian L <sebastian.lara6087@alumnos.udg.mx>
2026-02-25 19:37:48 -0600

    pistol smoke and max trace distance

--- Source/Blaster/Weapon/HitScanWeapon.cpp
+++ Source/Blaster/Weapon/HitScanWeapon.cpp
@@ -6,49 +6,60 @@
 #include "NiagaraFunctionLibrary.h"
 #include "Engine/SkeletalMeshSocket.h"
 #include "Kismet/GameplayStatics.h"
+#include "NiagaraComponent.h"
 
 void AHitScanWeapon::Fire(const FVector& HitTarget)
 {
 	Super::Fire(HitTarget);
-	
+
 	const auto World = GetWorld();
 	if (!World) return;
-	
+
 	const auto OwnerPawn = Cast<APawn>(GetOwner());
 	if (!OwnerPawn) return;
-	
-	const auto OwnerController = OwnerPawn->GetController();
-	if (!OwnerController) return;
-	
+
+	const auto InstigatorController = OwnerPawn->GetController();
+
 	const USkeletalMeshSocket* MuzzleFlashSocket = GetMesh()->GetSocketByName(MuzzleSocketName);
 	if (!MuzzleFlashSocket) return;
-	
+
 	const FTransform SocketTransform = MuzzleFlashSocket->GetSocketTransform(GetMesh());
 	const FVector Start = SocketTransform.GetLocation();
-	const FVector End = Start + (HitTarget - Start) * 1.25f; // Multiply by 1.25 to make sure end location reaches hit location.
-	
-	FHitResult Hit;
+	const FVector End = Start + (HitTarget - Start).GetSafeNormal() * MaxTraceDistance;
+	FVector BeamEnd = End;
 
-	World->LineTraceSingleByChannel(Hit, Start, End, ECollisionChannel::ECC_Visibility);
+	// Trace will ignore owner.
+	FHitResult Hit;
+	FCollisionQueryParams CollisionParams;
+	CollisionParams.AddIgnoredActor(OwnerPawn);
+
+	World->LineTraceSingleByChannel(Hit, Start, End, ECollisionChannel::ECC_Visibility, CollisionParams);
 	if (Hit.bBlockingHit) // There was an impact.
 	{
-		if (HasAuthority()) // Apply damage.
+		BeamEnd = Hit.ImpactPoint;
+		if (HasAuthority() && InstigatorController) // Apply damage.
 		{
 			UGameplayStatics::ApplyDamage(
 				Hit.GetActor(),
 				Damage,
-				OwnerController,
+				InstigatorController,
 				this,
 				UDamageType::StaticClass()
-				);
+			);
 		}
 		if (ImpactEffect)
 		{
-			UNiagaraFunctionLibrary::SpawnSystemAtLocation(this,
-				ImpactEffect,
-				Hit.ImpactPoint,
-				Hit.ImpactNormal.Rotation());
+			UNiagaraFunctionLibrary::SpawnSystemAtLocation(World,
+			                                               ImpactEffect,
+			                                               Hit.ImpactPoint,
+			                                               Hit.ImpactNormal.Rotation());
 		}
-		DrawDebugSphere(World, Hit.ImpactPoint, 20.f, 12, FColor::Purple, true);
+	}
+	if (BeamEffectLegacy)
+	{
+		UParticleSystemComponent* BeamLegacy = UGameplayStatics::SpawnEmitterAtLocation(
+			World, BeamEffectLegacy, SocketTransform);
+
+		BeamLegacy->SetVectorParameter(FName("Target"), BeamEnd);
 	}
 }
--- Source/Blaster/Weapon/HitScanWeapon.h
+++ Source/Blaster/Weapon/HitScanWeapon.h
@@ -7,7 +7,7 @@
 #include "HitScanWeapon.generated.h"
 
 class UNiagaraSystem;
-
+class UParticleSystem;
 /*
  * A type of weapon that casts line traces to emulate projectile hits.
  * NOTE: this type of weapon applies damage to the actor it hits, no matter
@@ -25,10 +25,16 @@ private:
 	UPROPERTY(EditAnywhere, meta = (ClampMin = 0.000001f))
 	float Damage = 10.f;
 	
+	UPROPERTY(EditAnywhere, meta = (ClampMin = 0.000001f))
+	float MaxTraceDistance = 100.f; 
+	
 	/* In the future, to implement different impact particles and sounds I could use the same approach
 	* I used in the Projectile class.
 	* For now, I will use only one impact particle.
 	 */
 	UPROPERTY(EditDefaultsOnly)
 	TObjectPtr<UNiagaraSystem> ImpactEffect;
+	
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<UParticleSystem> BeamEffectLegacy; // A legacy particle because I need to set a vector parameter and converting to niagara does not support making that parameter (I would have to make the parameter setup from scratch).
 };
